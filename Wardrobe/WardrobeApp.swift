6479293b8476422ed0f5206ca2a24a2f7587d19b
rashomon <shawon.ashraf@outlook.com>
2026-04-23 19:49:08 +0200

    Initial Commit

---
+++ Wardrobe/WardrobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardrobeApp.swift
+//  Wardrobe
+//
+//  Created by Shawon Ashraf on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WardrobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
